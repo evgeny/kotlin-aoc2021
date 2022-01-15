decccdf4f8f3991393a435e3eb6dbadd9b8e723d
Evgeny Zinovyev <e.zinovyev@gmail.com>
2022-01-15 17:22:03 +0100

    day6 - part2 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,9 @@
 fun main() {
+
+    val cache = Array(257) { mutableMapOf<Int, Long>() }
+
     fun part1(input: List<String>): Int {
-        var fishes: List<Int> = input[0].split(",").map { it.toInt() }.toList()
+        var fishes: List<Int> = input[0].split(",").map { it.toInt() }
 
         for (day in 0 until 80) {
             var newFishesWereBorn = 0
@@ -13,18 +16,36 @@ fun main() {
                 }
             }
 
-            fishes = fishes +  IntArray(newFishesWereBorn) { 8 }.toList()
+            fishes = fishes + IntArray(newFishesWereBorn) { 8 }.toList()
         }
 
 
         return fishes.toList().size
     }
 
-    fun part2(input: List<String>): Int {
-        return -1
+    fun fishOffspring(days: Int, startDay: Int): Long {
+        val cached = cache[days][startDay]
+
+        if (cached != null) return cached
+        if (days <= startDay) return 1
+
+        val remainingDays = days - startDay - 1
+
+        var fishes = 1L
+        for (d in remainingDays downTo 0 step 7) {
+            fishes += fishOffspring(d, 8)
+        }
+
+        cache[days][startDay] = fishes
+        return fishes
     }
 
-    val input = readInput("Day06_test")
+    fun part2(input: List<String>): Long {
+        val fishes: List<Int> = input[0].split(",").map { it.toInt() }
+        return fishes.sumOf { fishOffspring(256, it) }
+    }
+
+    val input = readInput("Day06")
     println(part1(input))
     println(part2(input))
 }
