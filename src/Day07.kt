57b8d6cfb4e172e71ead9c00a0ab0b3f3b0902a8
Evgeny Zinovyev <e.zinovyev@gmail.com>
2022-01-16 19:01:02 +0100

    day7 - part2 solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,10 +1,11 @@
 import kotlin.math.abs
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 fun main() {
     fun part1(input: List<String>): Int {
         val values = input[0].split(",").map { it.toInt() }
-        val mean = values.average()
+        val mean = values.average().roundToInt()
         val median = values.sorted().let {
             if (it.size.mod(2) == 0) {
                 (it[it.size / 2] + it[it.size / 2 - 1]) / 2
@@ -14,7 +15,7 @@ fun main() {
         }
 
         var min = Int.MAX_VALUE
-        for (i in median..mean.toInt()) {
+        for (i in median..mean) {
             val sum = values.sumOf { abs(it - i) }
             min = min(min, sum)
         }
@@ -23,11 +24,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return -1
+        val values = input[0].split(",").map { it.toInt() }.sorted()
+        val mean = values.average().roundToInt()
+        val median = values.let {
+            if (it.size.mod(2) == 0) {
+                (it[it.size / 2] + it[it.size / 2 - 1]) / 2
+            } else {
+                it[it.size / 2]
+            }
+        }
+
+        var min = Int.MAX_VALUE
+        for (i in median..mean) {
+            val sum = values.sumOf {
+                val distance = abs(it - i)
+                (distance * (distance + 1)) / 2
+            }
+            min = min(min, sum)
+        }
+
+        return min
     }
 
-//    val testInput = listOf("16,1,2,0,4,2,7,1,2,14")
     val input = readInput("Day07")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
